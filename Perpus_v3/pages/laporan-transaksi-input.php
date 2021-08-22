8ab0187d9eae4276d820cfe320db8a96476d2f20
amallindask <89288196+amallindask@users.noreply.github.com>
2021-08-22 11:35:49 +0700

    Finish upload file

---
+++ Perpus_v3/pages/laporan-transaksi-input.php
@@ -0,0 +1,32 @@
+<div id="label-page"><h3>Input Laporan Transaksi</h3></div>
+<div id="content">
+	<form action="proses/anggota-input-proses.php" method="post" enctype="multipart/form-data">
+	
+	<table id="tabel-input">
+		<tr>
+			<td class="label-formulir">ID Transaksi</td>
+			<td class="isian-formulir"><input type="text" name="id_transaksi" class="isian-formulir isian-formulir-border"></td>
+		</tr>
+		<tr>
+			<td class="label-formulir">ID Anggoata</td>
+			<td class="isian-formulir"><input type="text" name="id_anggota" class="isian-formulir isian-formulir-border"></td>
+		</tr>
+		<tr>
+			<td class="label-formulir">Kode Buku</td>
+			<td class="isian-formulir"><input type="text" name="id_buku" class="isian-formulir isian-formulir-border"></td>
+		</tr>
+		<tr>
+			<td class="label-formulir">Tanggal Pinjam</td>
+			<td class="isian-formulir"><input type="date" name="tgl_pinjam" class="isian-formulir isian-formulir-border"></td>
+		</tr>
+		<tr>
+			<td class="label-formulir">Tanggal Kembali</td>
+			<td class="isian-formulir"><input type="date" name="tgl_kembali" class="isian-formulir isian-formulir-border"></td>
+		</tr>
+		<tr>
+			<td class="label-formulir"></td>
+			<td class="isian-formulir"><input type="submit" name="simpan" value="Simpan" class="tombol"></td>
+		</tr>
+	</table>
+	</form>
+</div>
