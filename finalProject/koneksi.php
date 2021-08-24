0b13d74aad6f3a8337f0fd89e3525f4eacee7f58
Wawan <85148532+wawann11@users.noreply.github.com>
2021-08-24 20:06:28 +0700

    Update koneksi.php

--- finalProject/koneksi.php
+++ finalProject/koneksi.php
@@ -2,7 +2,7 @@
 	$host = "localhost";
 	$username = "root";
 	$password = "";
-	$database = "crud";
+	$database = "peserta";
 	
 	$pdo = new PDO ('mysql:host='.$host.';dbname='.$database, $username, $password);
 ?>
