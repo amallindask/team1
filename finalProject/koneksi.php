16a7bb6341529b486c7e6b56ffde28ff2e05dc93
amarzyo <87264283+amarzyo@users.noreply.github.com>
2021-08-26 01:46:01 +0700

    Delete finalProject directory

--- finalProject/koneksi.php
+++
@@ -1,8 +0,0 @@
-a<?php
-	$host = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "peserta"
-	
-	$pdo = new PDO ('mysql:host='.$host.';dbname='.$database, $username, $password);
-?>
