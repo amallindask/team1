9e028c2db5a11a7ecfed4a1abdb69132b63f875c
Wawan <85148532+wawann11@users.noreply.github.com>
2021-08-25 21:28:11 +0700

    Update form_ubah.php

--- finalProject/team1Final/form_ubah.php
+++ finalProject/team1Final/form_ubah.php
@@ -1,12 +1,45 @@
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="tampilan-simpan.css">
-    <title>Pemberian Sertifikat | CRUD</title>
-</head>
+<?php
+	require 'koneksi.php';
 
-<body>    
+	if(isset($_POST['simpan']))
+	{
+		$id             = $_POST['id'];
+        $nama           = $_POST['nama'];
+        $jenis_kelamin  = $_POST['jenis_kelamin'];
+        $tgl_kegiatan   = $_POST['tgl_kegiatan'];
+        $alamat         = $_POST['alamat'];
+        $peran          = $_POST['peran'];
+        $tgl_sertifikat = $_POST['created_at'];
+
+	 	mysqli_query($conn, "UPDATE peserta SET nama='$nama', jenis_kelamin='$jenis_kelamin', tgl_kegiatan='$tgl_kegiatan', alamat='$alamat', peran='$peran', tgl_sertifikat='$created_at' WHERE id_peserta=$id");
+		header("Location: lihat.php");
+	}
+?>
+
+<?php
+    $id = $_GET['id'];
+     
+    $peserta = mysqli_query($conn, "SELECT * FROM peserta WHERE id_peserta = $id");
+     
+    while($pst = mysqli_fetch_array($peserta))
+    {
+        $nama           = $pst['nama'];
+        $jenis_kelamin  = $pst['jenis_kelamin'];
+        $tgl_kegiatan   = $pst['tgl_kegiatan'];
+        $alamat         = $pst['alamat'];
+        $peran          = $pst['peran'];
+        $tgl_sertifikat = $pst['created_at'];
+    }
+?>
+
+<html>
+<head>    
+    <title>Edit Data Peserta</title>
+</head>
+ 
+<body>
     <h1 id="header">Ubah Data Sertifikat</h1>
-    <form method="post" action="proses_simpan.php">
+    <form method="post" action="lihat.php">
         <table>
             <tr>
                 <td>No Sertifikat</td>
@@ -14,7 +47,7 @@
             </tr>
             <tr>
                 <td>Nama</td>
-                <td><input type="text" name="nama"></td>
+                <td><input type="text" name="nama" required></td>
             </tr>
             <tr>
                 <td>Jenis Kelamin</td>
@@ -24,29 +57,28 @@
                 </td>
             </tr>
             <tr>
-                <td>Tanggal Sertifikat</td>
-                <td><input type="date" name="tgl_sertifikat"></td>
+                <td>Tanggal Kegiatan</td>
+                <td><input type="date" name="tgl_kegiatan" required></td>
             </tr>
             <tr>
                 <td>Alamat</td>
-                <td><textarea name="alamat" rows="5"></textarea></td>
+                <td><textarea name="alamat" rows="2" required></textarea></td>
             </tr>
             <tr>
                 <td>Peran</td>
                 <td>
-                    <input type="radio" name="peran" value="Narasumber"> Narasumber
-                    <input type="radio" name="peran" value="Peserta"> Peserta
+                    <input type="radio" name="peran" value="Pemateri" required><label>Pemateri</label>
+                    <input type="radio" name="peran" value="Peserta" required><label>Peserta</label>
                 </td>
             </tr>
             <tr>
-                <td>Tema</td>
-                <td><textarea name="tema" rows="5"></textarea></td>
+                <td>Tanggal Sertfikat</td>
+                <td><input type="date" name="created_at" required></td>
             </tr>
         </table>
-        
         <hr>
         <input type="submit" class="simpan" value="Simpan">
-        <a href="index.php"><input type="button" class="batal" value="Batal"></a>
+        <a href="lihat.php"><input type="button" class="batal" value="Batal"></a>
     </form>
 </body>
 </html>
