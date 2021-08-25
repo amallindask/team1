64e8fc2f94071775754bb862cb6d60c32d90bcab
amarzyo <adhiezt28@gmail.com>
2021-08-26 02:13:15 +0700

    reupload

---
+++ team1Final/sertif/insert.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+
+<body>
+ 
+	<?php
+	include 'koneksi.php';
+ 
+	$sql = "INSERT INTO Students (head, head2, head3, ket, detail, detail2, ttd, ttd2) VALUES ('KEMENTRIAN PENDIDIKAN DAN KEBUDAYAAN', 'POLITEKNIK NEGERI CILACAP', 'PUSAT PENELITIAN DAN PENGABDIAN KEPADA MASYARAKAT', 'SERTIFIKAT', 'Atas peran serta sebagai ', ' dalam kegiatan Webinar dengan tema “Diskusi Strategi Membangun Keunggulan Kompetitif Dosen dalam Bidang Penelitan” yang diselenggarakan pada 28 Juli 2020, dalam kurun waktu 4 (empat) jam pertemuan yang diselengarakanoleh Pusat Penelitian dan Pengabdian kepada Masyarakat (P3M) Politeknik Negeri Cilacap.', 'Dr. Ir. Aris Tjahyanto M.Kom','Ganjar Ndaru Ikhtiagung, S.E., M.M.')";
+	?>
+</body>
+</html>
